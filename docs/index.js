942e75d9a2de3edc5ee6fc3e9a12eabe13a144ae
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2024-11-30 17:38:06 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,10 +3,11 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+const divHeader = document.createElement("div");
+const divBody = document.createElement("div");
+
 window.addEventListener("load", () => {
-  const divHeader = document.createElement("div");
   document.body.appendChild(divHeader);
-  const divBody = document.createElement("div");
   document.body.appendChild(divBody);
 
   if (window.crossOriginIsolated) {
